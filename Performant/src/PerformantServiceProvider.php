9cf0ae94252a242b90544b554be01867fdee0cb2
KimLangholz <kontakt@kimlangholz.dk>
2020-01-13 11:01:25 +0100

    Initial commit of the first Analyzer module

---
+++ Performant/src/PerformantServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace JustIversen\Performant;
+
+use JustIversen\Performant\AnalyzeCode;
+use Illuminate\Support\ServiceProvider;
+
+class PerformantServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        if($this->app->runningInConsole()) {
+            $this->registerArtisanCommands();
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+    public function registerArtisanCommands():void
+    {
+        $this->commands([
+            AnalyzeCode::class,
+        ]);
+    }
+}
